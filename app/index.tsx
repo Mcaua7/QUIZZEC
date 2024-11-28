28230875ef10ed96742b733b9e4e21f4a7efc8b2
MATHEUS CAUA <cauaalves@irislab.ce.gov.br>
2024-11-28 17:03:29 -0300

    Finalizando a instalação do nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
+    <View className="bg-red-500">
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
         <Text style={styles.subtitle}>This is the first page of your app.</Text>
