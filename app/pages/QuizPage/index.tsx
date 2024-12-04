d47bf1d01a391580fca60e6abe5be9e614c94ab3
Renan Soares Souza <renansoares.oficiall@gmail.com>
2024-12-04 14:25:46 -0300

    adicionando página do quiz para a Rota

---
+++ app/pages/QuizPage/index.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+
+export default function QuizPage() {
+  return (
+    <View>
+      <View></View>
+    </View>
+  );
+}
