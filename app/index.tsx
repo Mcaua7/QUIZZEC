c5b1b2fe319afe204899978fbb4583d8206c3193
Renan Soares Souza <renansoares.oficiall@gmail.com>
2024-12-11 13:40:16 -0300

    finalizado

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
-import { router } from "expo-router";
-import { View, Image } from "react-native";
+import { View } from "react-native";
+import ListQuiz from "./pages/listQuiz";
 
 export default function Page() {
-  setTimeout(() => router.push({ pathname: "pages/listQuiz" }), 1000);
-
   return (
-    <View className="h-full w-screen justify-center items-center bg-[#412E8B]">
-      <Image
-        className="h-52 w-52"
-        source={require("./../assets/QUIZZEC.png")}
-      />
+    <View className="h-full w-screen">
+      <ListQuiz />
     </View>
   );
 }
