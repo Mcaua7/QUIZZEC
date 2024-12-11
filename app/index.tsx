c602c9f8e93a56d29e0c4064b9548fe07fc3faf1
Renan Soares Souza <renansoares.oficiall@gmail.com>
2024-12-11 13:42:33 -0300

    finalizado

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { View } from "react-native";
-import ListQuiz from "./pages/listQuiz";
+import ListQuiz from "c:/Users/Iris Lab/Documents/QUIZZEC/app/pages/listQuiz/index";
 
 export default function Page() {
   return (
